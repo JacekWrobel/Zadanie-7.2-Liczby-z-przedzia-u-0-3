cc17cf153ecd9c11516f5ab47cc77f45cf198fab
Jacek <ja77co@wp.pl>
2019-05-22 20:33:36 +0200

    Zadanie 7.2 Liczby z przedziału 0-3

---
+++ src/DoubleNumbers.java
@@ -0,0 +1,16 @@
+public class DoubleNumbers {
+    public static void main(String[] args) {
+        double i = 0;
+        while (i < 3.1) {
+            System.out.printf("%.1f, ", i);
+            i = i + 0.1;
+        }
+        System.out.println();
+        i = 0;
+        do {
+            System.out.printf("%.1f, ", i);
+            i = i + 0.1;
+        } while (i < 3.1);
+
+    }
+}
